571a26f886ef8e53fae36898ae8c518a5395b2dd
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-06-08 21:35:39 +0100

    After consulting ChatGPT, there were a few ways to complete this project. All of which required different setups for the configuration of the database being used and how to use it all within the code. While I sit and procrastinate a little, I probably would go for the db to be an internal sqlite database instead. This should help for usability when someone tries to download and clone from online.

--- services/db.py
+++ services/db.py
@@ -14,25 +14,4 @@ tasks_table = database["todo-list"]
 users_table = database['user_logins']
 
 def register_user(user):
-    # id - custom field
-    # name
-    # email
-    # password
-    # createdAt
-    # updatedAt
-    
-def login_user(user):
-    # email
-    # password
-    # if auth, token
-    # else error
-
-def add_task(task):
-    # user_id
-    # task title
-    # task_status
-    # task_due
-    # descriptipn
-    # createdAt
-    # updatedAt
     
